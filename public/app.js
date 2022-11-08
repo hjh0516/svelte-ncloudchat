684d08f168b3768b42350afe875557fa59510f16
Teddy <note@kakao.com>
2022-11-08 16:06:39 +0900

    fix

--- public/app.js
+++ public/app.js
@@ -13,7 +13,6 @@ let isMobile = {
 };
 
 function godetail() {
-  alert("godetail");
   if (isMobile.any()) {
     if (isMobile.Android()) {
       window.emoApp.godetail();
@@ -24,7 +23,6 @@ function godetail() {
 }
 
 function gohome() {
-  alert("gohome");
   if (isMobile.any()) {
     if (isMobile.Android()) {
       window.emoApp.gohome();
@@ -35,6 +33,5 @@ function gohome() {
 }
 
 function showsetting() {
-  alert("showsetting");
   showSettingModal = true;
 }
