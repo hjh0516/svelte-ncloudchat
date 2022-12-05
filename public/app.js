ebb17d88e7e0a17176cf982a6f541e801325bd57
thomas <hjh8684@gmail.com>
2022-12-05 21:29:11 +0900

    bug fix

--- public/app.js
+++ public/app.js
@@ -72,7 +72,7 @@ function goback() {
     !userInfo2
   ) {
     if (isMobile.Android() && window.emoApp) {
-      window.emoApp?.gomain();
+      window.emoApp?.goMain();
       // location.href = "gomain://";
     }
     return;
