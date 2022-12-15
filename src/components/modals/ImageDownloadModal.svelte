83e430bcf079878aafbc1da274eaf413fefcf2c6
thomas <hjh8684@gmail.com>
2022-12-15 11:32:40 +0900

    test

--- src/components/modals/ImageDownloadModal.svelte
+++ src/components/modals/ImageDownloadModal.svelte
@@ -16,7 +16,7 @@
     const image_url = VITE_ARCHIVE_URL + item.image_url;
     
     if (isMobile.Android() && window.emoApp) {
-      window.emoApp?.godownload();
+      window.emoApp?.godownload({url : image_url});
     }else{
       const res = await fetch(image_url);
       const blob = await res.blob();
